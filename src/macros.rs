cd32464b8a7924b6496544072b0c4836f687fe6b
Cryptjar <cryptjar@wag-web.de>
2019-04-16 15:45:45 +0200

    Add docs to macro.

--- src/macros.rs
+++ src/macros.rs
@@ -1,10 +1,105 @@
 //! Provides macros easier printing with colors and styles.
+//!
 //! See: https://github.com/murarth/mortal/issues/7
 
 
 
 
-/// Writes on term
+/// Writes on given terminal using themes.
+///
+/// # Syntax
+///
+/// The base syntax of this macro is:
+///
+/// ```ignore
+/// term_write!( [lock] <terminal> ; [theming|output] ... )
+/// ```
+///
+/// First there is the optional keyword `lock` followed by the identifier
+/// (aka name) of the terminal variable. This variable must be of type
+/// [`Terminal`] or [`TerminalWriteGuard`]. If the type of that
+/// variable is `Terminal` the keyword `lock` may be specified too.
+/// After the terminal variable follow, separated by a semicolon `;`, either
+/// _theme specifications_ or _output instructions_, or multiple of them.
+///
+/// The _theme specifications_ are fenced by square bracket `[ ]`. Within these
+///  is either of the following:
+/// - a foreground color: small caps color name such as (`black`, `blue`,
+///   `cyan`, `green`, `magenta`, `red`, `white`, `yellow`)
+/// - a background color: similar to foreground but with a leading hash `#` such
+///   as (`#black`, `#blue`, `#cyan`, `#green`, `#magenta`, `#red`, `#white`,
+///   `#yellow`)
+/// - an additive style specifier: a small caps style name such as (`bold`,
+///   `italic`, `underline`, `reverse`)
+/// - a subtractive style specifier: similar to additive but with a
+///   leading exclamation mark such as (`!bold`, `!italic`, `!underline`,
+///   `!reverse`)
+/// - a reset specifier either of `reset`, `!fg`, `!bg`, `!sty`
+/// - foreground variable: `fg=` plus the name of a variable
+/// - background variable: `bg=` plus the name of a variable
+/// - style variable: `sty=` plus the name of a variable
+/// - theme variable: `=` plus the name of a variable
+///
+/// The _output instructions_ may be either of the following:
+/// - a literal such as a string `"stuff"` or integer `42`
+/// - a Rust `std::fmt` format string with arguments fenced in
+///   parentheses `( )` such as `("some {}", "text")`
+/// - a `Display` shortcut for the format string `"{}"`, which is an expression
+///   enclosed in parentheses with a leading colon `(: )` such as `(: true)`
+/// - a `Debug` shortcut for the format string `"{:?}"`, which is an expression
+///   enclosed in parentheses with a leading question mark `(? )` such as
+///   `(? true)`. 
+///
+/// [`Terminal`]: ./terminal/struct.Terminal.html
+/// [`TerminalWriteGuard`]: terminal/struct.TerminalWriteGuard.html
+///
+///
+/// # Locking
+///
+/// The terminal write lock will be acquired as required. If the first argument
+/// is of type `TerminalWriteGuard` no locking will be performed since the guard
+/// already holds the lock. If the first argument is of type `Terminal` then
+/// the keyword `lock` may be prefixed. If `lock` is given, then the macro will
+/// acquire the terminal guard before any output and write out all output while
+/// holding it. If the `lock` prefix is not given, each output fragment will lock
+/// the terminal independently, which might lead to actual
+/// fragmented output if there is concurrent thread writing to the terminal.
+///
+/// Notice that if the `TerminalWriteGuard` is held by the current thread,
+/// then this macro must not be called with the `Terminal`. Otherwise a deadlock
+/// will occur.
+///
+///
+/// # Examples
+///
+/// ```
+/// #[macro_use] extern crate mortal;
+/// use mortal::Terminal;
+/// use mortal::Theme;
+/// use mortal::Color;
+/// use mortal::Style;
+///
+/// let term = Terminal::new().unwrap();
+/// // Simple output example
+/// term_write!(term; "Hello world");
+///
+/// // Writing format strings
+/// term_write!(term; ("Number #{}", 42));
+///
+/// // Using keywords
+/// term_write!(term; [blue] "A blue " [bold] "world");
+///
+/// // Using variables
+/// let c = Color::Green;
+/// term_write!(term; "Just " [fg=c] (? c)); // short cut for ("{:?}", c)
+///
+/// // Using themes
+/// let theme = Theme::default().fg(Color::Red).style(Style::BOLD);
+/// term_write!(term; [=theme] "Red and Bold");
+/// ```
+///
+/// Further examples can be found in the examples folder of Mortal.
+///
 #[macro_export]
 macro_rules! term_write {
 	// Lock prefix
@@ -206,7 +301,7 @@ macro_rules! term_write {
 }
 
 
-/// Same as term_write but adds a new line at the end.
+/// Same as term_write macro but adds a new line at the end.
 #[macro_export]
 macro_rules! term_writeln {
 	( lock $term:ident; $( $rest:tt )* ) => {
